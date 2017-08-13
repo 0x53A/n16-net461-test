1e124b46bab2a67678afd5066c525febb1048368
Lukas Rieger <lr@live.de>
2017-08-13 20:42:25 +0200

    Add project files.

---
+++ n16/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace n16
+{
+    public class Class1
+    {
+        public static int Hello()
+        {
+            return 1;
+        }
+    }
+}
---
+++ net461/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace net461
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            var i = n16.Class1.Hello();
+            Console.WriteLine(i);
+        }
+    }
+}
